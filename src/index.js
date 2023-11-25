cbd09838d51e0d0be8d118997ef109eeb3d70fce
Nyi Ye Zin <nyiyezin11@gmail.com>
2023-11-25 18:05:19 +0630

    add basic ui

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import Library from './Library.js';
+import Book from './Book.js';
+import {
+  bookList,
+  bookForm,
+  bookTitleInput,
+  bookAuthorInput,
+} from './variables.js';
+
+const bookLibrary = new Library();
+
+bookLibrary.books.forEach((book) => {
+  bookLibrary.renderBook(book);
+});
+
+bookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const bookTitle = bookTitleInput.value;
+  const bookAuthor = bookAuthorInput.value;
+  if (!bookTitle || !bookAuthor) return;
+  const newBook = new Book(bookTitle, bookAuthor);
+  bookLibrary.addBook(newBook);
+  bookLibrary.renderBook(newBook);
+});
+
+bookList.addEventListener('click', (event) => {
+  if (!event.target.matches('[data-button-delete]')) return;
+  const parent = event.target.closest('#bookWrapper');
+  const { bookId } = parent.dataset;
+  parent.remove();
+  bookLibrary.removeBook(bookId);
+});
